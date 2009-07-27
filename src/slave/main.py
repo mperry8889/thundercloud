1256aeefac99b616912134f2879d58698ba7a661
matt <none@none>
2009-07-26 22:41:41 -0400

    reorg and rename some files
    --HG--
    rename : src/slave/main.py => src/thunderclient/main.py
    rename : src/slave/prototype.py => src/thunderclient/prototype.py

--- src/slave/main.py
+++
@@ -1,5 +0,0 @@
-from twisted.internet import reactor
-from thunderclient.controller.restApi import createRestApi
-
-reactor.listenTCP(7000, createRestApi())
-reactor.run()
---
+++ src/thunderclient/main.py
@@ -0,0 +1,10 @@
+from twisted.internet import reactor
+from thunderclient.controller.restApi import createRestApi
+import logging as log
+
+log.basicConfig(level=log.DEBUG)
+
+port = 7000
+log.debug("Listening on port %s, starting reactor" % port)
+reactor.listenTCP(port, createRestApi())
+reactor.run()
