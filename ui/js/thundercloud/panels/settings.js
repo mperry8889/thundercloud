f388bf68dff033348cc78a7b7b043feedeb1e9c3
matt <none@none>
2009-08-26 00:21:56 -0400

    Get rid of the old UI

--- ui/js/thundercloud/panels/settings.js
+++
@@ -1,8 +0,0 @@
-tc.panel.settings = new Object();
-
-$(document).ready(function() {
-	$("#settings-clientsPerSec-input").val("10");	
-	$("#settings-maxTransfer-input").val("10");
-	$("#settings-duration-input").val("30");	
-	$("#settings-statsInterval-input").val("1");
-});
