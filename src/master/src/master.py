c1a6ca1b3ce009b771783b4d09d0200e4abad52f
matt <none@none>
2009-09-09 17:08:48 -0400

    Some basic support for adding slaves on master startup

--- src/master/src/master.py
+++ src/master/src/master.py
@@ -1,6 +1,9 @@
 from twisted.internet import reactor
 from thunderserver.restApi import createRestApi
 from thundercloud import config
+from thunderserver.orchestrator import Orchestrator
+from thundercloud.spec.slave import SlaveSpec
+from twisted.internet.defer import DeferredList
 import logging
 import sys
 
@@ -10,10 +13,43 @@ except:
     print "No config file specified, exiting"
     sys.exit(1)
 
+
+def registerSlaveCallback(value):
+    for (success, slaveId) in value:
+        if success == False:
+            log.error("Error adding slave")
+        else:
+            log.info("Slave %s connected" % slaveId)
+
+def registerSlaveErrback():
+    pass
+
 logging.basicConfig(level=eval("logging.%s" % config.parameter("log", "level")))
 log = logging.getLogger("main")
 
+
+# add slaves in the INI file if they're around and add-able
+slaves = {}
+for (key, val) in config.section("slave"):
+    slaveNo = key[:key.find(".")]
+    attr = key[key.find(".")+1:]
+    
+    try:
+        setattr(slaves[slaveNo], attr, val)
+    except KeyError:
+        slaves[slaveNo] = SlaveSpec()
+        setattr(slaves[slaveNo], attr, val)
+    
+    
+deferreds = []
+for slaveSpec in slaves.itervalues():
+    log.info("Connecting slave: %s" % slaveSpec)
+    deferreds.append(Orchestrator.registerSlave(slaveSpec))
+
+deferredList = DeferredList(deferreds)
+deferredList.addCallback(registerSlaveCallback)
+
 log.debug("Listening on port %s, starting reactor" % config.parameter("network", "port"))
-reactor.listenTCP(int(config.parameter("network", "port")), createRestApi())
+reactor.listenTCP(config.parameter("network", "port", type=int), createRestApi())
 
 reactor.run()
